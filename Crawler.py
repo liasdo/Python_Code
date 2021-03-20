d7c463200f3f9c88c6dd781da951460e6edea2ab
liasdo <2231978143@qq.com>
2021-03-20 21:02:30 +0800

    使用BeautifulSoup找到图片链接

--- Crawler.py
+++ Crawler.py
@@ -1,12 +1,21 @@
-import requests,lxml
-import os
+import requests, os
 from bs4 import BeautifulSoup
+
+
+proxies = {
+  "http": "http://127.0.0.1:7890",
+  "https": "http://127.0.0.1:7890",
+}
+
+
 
 page_num = input("请输入页数：")
 url_head = "https://nhentai.net/g/"
 url = url_head + str(page_num) + '/1'
-data = requests.get(url).text
-#soup = BeautifulSoup(data, 'html.parser')
-#pic_label = soup.find_all('img', width="1280")
+data = requests.get(url, proxies=proxies).text
+soup = BeautifulSoup(data, 'html.parser')
+pic_label = soup.find_all('img', width="1280")
+for i in pic_label:
+    pic_link = i['src']
 
-print(data)
+print(pic_link)
