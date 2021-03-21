285a0513490c237b6556ca4e5f2dac9dca7c9ada
liasdo <2231978143@qq.com>
2021-03-21 22:12:58 +0800

    nhentai本子爬取未重构版

--- Crawler.py
+++ Crawler.py
@@ -1,15 +1,16 @@
 import requests, os
 from bs4 import BeautifulSoup
 from wget import download
+from urllib.request import urlretrieve
 
 proxies = {
   "http": "http://127.0.0.1:7890",
   "https": "http://127.0.0.1:7890",
 }
 
-os.chdir("D:\H\本子\[Ginhaha] Formidable to Tea Time + SP (Azur Lane) [Chinese]")
-url_head = "https://nhentai.net/g/291317/"
-page_num = 1
+os.chdir("D:\H\本子\欲求王")
+url_head = "https://nhentai.net/g/203256/"
+page_num = 169
 while 1:
     try:
         url = url_head + str(page_num)
@@ -19,12 +20,13 @@ while 1:
             print("爬取完成，告辞")
             break
         soup = BeautifulSoup(data, 'html.parser')
-        pic_label = soup.find_all('img', width="1280")
+        pic_label = soup.find_all('img', width="720")
         for i in pic_label:
             pic_link = i['src']
-        print("正在爬取",pic_link)
         pic_name = str(page_num) + ".jpg"
-        download(pic_link, out=pic_name)
+        #download(pic_link, out=pic_name)
+        urlretrieve(pic_link, pic_name)
+        print("已爬取：", pic_link)
         page_num = page_num + 1
     except:
         print("出现迷之问题")
--- test.py
+++ test.py
@@ -1,8 +1,33 @@
-import requests
-import os
+import requests, os
 from bs4 import BeautifulSoup
+from wget import download
+from urllib.request import urlretrieve
 
-url = "https://www.baidu.com"
-res = requests.get(url)
-data = res.text
-print(data)
+os.environ["http_proxy"] = "http://127.0.0.1:7890"
+os.environ["https_proxy"] = "http://127.0.0.1:7890"
+
+
+
+os.chdir("D:\H\本子\欲求王")
+url_head = "https://nhentai.net/g/203256/"
+page_num = 337
+while 1:
+    try:
+        url = url_head + str(page_num)
+        data = requests.get(url).text
+        status_code = requests.get(url).status_code
+        if status_code > 200:
+            print("爬取完成，告辞")
+            break
+        soup = BeautifulSoup(data, 'html.parser')
+        pic_label = soup.find_all('img', width="720")
+        for i in pic_label:
+            pic_link = i['src']
+        pic_name = str(page_num) + ".jpg"
+        #download(pic_link, out=pic_name)
+        urlretrieve(pic_link, pic_name)
+        print("已爬取：", pic_link)
+        page_num = page_num + 1
+    except:
+        print("出现迷之问题")
+        break
